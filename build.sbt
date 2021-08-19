ccc575eed6e92a42a9e0a5234625098d61b9ecc8
dedkot <dedkot01@gmail.com>
2021-08-20 00:57:10 +0300

    added atmosphere

--- build.sbt
+++ build.sbt
@@ -1,18 +1,22 @@
-val ScalatraVersion = "2.8.0"
-
 ThisBuild / scalaVersion := "2.13.4"
 ThisBuild / organization := "org.dedkot"
+
+val ScalatraVersion = "2.7.+"
 
 lazy val planningPoker = (project in file("."))
   .settings(
     name := "PlanningPokerWebApp",
     version := "0.1",
     libraryDependencies ++= Seq(
+      "org.json4s" %% "json4s-jackson" % "3.6.7",
       "org.scalatra" %% "scalatra" % ScalatraVersion,
-      "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
-      "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime",
-      "org.eclipse.jetty" % "jetty-webapp" % "9.4.35.v20201120" % "container",
-      "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+      "org.scalatra" %% "scalatra-json" % ScalatraVersion,
+      "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+      "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % Test,
+      "org.scalatra" %% "scalatra-atmosphere" % ScalatraVersion,
+      "ch.qos.logback" % "logback-classic" % "1.2.3" % Provided,
+      "org.eclipse.jetty" % "jetty-webapp" % "9.4.35.v20201120" % Provided,
+      "javax.servlet" % "javax.servlet-api" % "3.1.0" % Provided
     ),
   )
 
--- src/main/scala/org/dedkot/app/MainServlet.scala
+++ src/main/scala/org/dedkot/app/MainServlet.scala
@@ -1,11 +1,19 @@
 package org.dedkot.app
 
+import org.json4s.JsonDSL._
+import org.json4s._
 import org.scalatra._
+import org.scalatra.atmosphere._
+import org.scalatra.json.{JValueResult, JacksonJsonSupport}
 
+import java.util.Date
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class MainServlet extends ScalatraServlet {
+class MainServlet extends ScalatraServlet with JValueResult with JacksonJsonSupport with AtmosphereSupport {
+
+  implicit protected val jsonFormats: Formats = DefaultFormats
 
   private var tables: Seq[String] = Seq.empty
 
@@ -33,16 +41,42 @@ class MainServlet extends ScalatraServlet {
     views.html.tables(username, tables)
   }
 
+  atmosphere("/tables") {
+    new AtmosphereClient {
+      def receive: AtmoReceive = {
+        case Connected =>
+          println("Client %s is connected" format uuid)
+          broadcast(("author" -> "Someone") ~ ("message" -> "joined the room") ~ ("time" -> new Date().getTime.toString), Everyone)
+
+        case Disconnected(ClientDisconnected, _) =>
+          broadcast(("author" -> "Someone") ~ ("message" -> "has left the room") ~ ("time" -> new Date().getTime.toString), Everyone)
+
+        case Disconnected(ServerDisconnected, _) =>
+          println("Server disconnected the client %s" format uuid)
+        case _: TextMessage =>
+          send(("author" -> "system") ~ ("message" -> "Only json is allowed") ~ ("time" -> new Date().getTime.toString))
+
+        case JsonMessage(json) =>
+          println("Got message %s from %s".format((json \ "message").extract[String], (json \ "author").extract[String]))
+          val msg = json merge ("time" -> new Date().getTime.toString: JValue)
+          broadcast(msg) // by default a broadcast is to everyone but self
+        //  send(msg) // also send to the sender
+      }
+    }
+  }
+
   post("/tables/new") {
     val table = params("table")
-    tables = tables.appended(table)
+    tables = tables :+ table
 
     redirect(s"/tables/$table")
   }
 
   get("/tables/:tableName") {
     val table = params("tableName")
-    views.html.table(table)
+
+    if (!tables.contains(table)) response.setStatus(404)
+    else views.html.table(table)
   }
 
 }
